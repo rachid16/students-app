9b1e6f1fc1bc4bf6e240765402af6e7162743a84
rachid <azeroual.rachid1@gmail.com>
2022-01-24 15:50:48 -0500

    first commit

---
+++ src/main/java/com/rachid/studentsapp/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.rachid.studentsapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/")
+    public String getMessage(){
+        return "Welcome to java!";
+    }
+}
---
+++ src/main/java/com/rachid/studentsapp/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.rachid.studentsapp.controller;
+
+public class StudentController {
+}
